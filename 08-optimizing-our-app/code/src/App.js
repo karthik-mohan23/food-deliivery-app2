65593f3f3e2aff32f744cafde7c3674b6192b188
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-06-27 16:53:59 +0530

    configured paths

--- 08-optimizing-our-app/code/src/App.js
+++ 08-optimizing-our-app/code/src/App.js
@@ -3,15 +3,42 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage";
+import Offer from "./components/Offer";
+import Help from "./components/Help";
+
 const App = () => {
   return (
     <>
       <Header />
-      <Body />
+      <Outlet />
     </>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/offers",
+        element: <Offer />,
+      },
+      {
+        path: "/help",
+        element: <Help />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
