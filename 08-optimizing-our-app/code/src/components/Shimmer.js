7a5b5e82aac6449f7e2d6439e9a8a1f2211c86e9
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-06-27 09:11:31 +0530

    added shimmer

--- 08-optimizing-our-app/code/src/components/Shimmer.js
+++ 08-optimizing-our-app/code/src/components/Shimmer.js
@@ -1,11 +1,16 @@
+import ShimmerCard from "./ShimmerCard";
+
 const Shimmer = () => {
   return (
     <section className="container ">
-      <div className="card-container shimmer">
-        <div className="shimmer-card">
-          <div className="sh-img-div"></div>
-          <div className="sh-name-div"></div>
-          <div className="shim-text-div"></div>
+      <div className="shimmer">
+        <div className="shimmer-banner"></div>
+        <div className="card-container shimmer-grid">
+          {Array(16)
+            .fill("")
+            .map((e, index) => {
+              return <ShimmerCard key={index} />;
+            })}
         </div>
       </div>
     </section>
---
+++ 08-optimizing-our-app/code/src/components/ShimmerCard.js
@@ -0,0 +1,10 @@
+const ShimmerCard = () => {
+  return (
+    <div className="shimmer-card">
+      <div className="sh-img-div"></div>
+      <div className="sh-name-div"></div>
+      <div className="shim-text-div"></div>
+    </div>
+  );
+};
+export default ShimmerCard;
